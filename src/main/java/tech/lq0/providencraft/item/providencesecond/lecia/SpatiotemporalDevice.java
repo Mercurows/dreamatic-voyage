c62093f8119971ebe38dbf28ac043f26fd73222c
17146 <1714673995@qq.com>
2023-10-25 00:07:35 +0800

    添加了时空约束装置的贴图和功能

--- src/main/java/tech/lq0/providencraft/item/providencesecond/lecia/SpatiotemporalDevice.java
+++ src/main/java/tech/lq0/providencraft/item/providencesecond/lecia/SpatiotemporalDevice.java
@@ -1,13 +1,52 @@
 package tech.lq0.providencraft.item.providencesecond.lecia;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.List;
 
 public class SpatiotemporalDevice extends Item {
     public SpatiotemporalDevice() {
-        super(new Properties().rarity(Rarity.EPIC).group(ModGroup.itemgroup));
+        super(new Properties().rarity(Rarity.EPIC).group(ModGroup.itemgroup).maxStackSize(1));
     }
 
+    @OnlyIn(Dist.CLIENT)
+    @ParametersAreNonnullByDefault
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add((new TranslationTextComponent("des.providencraft.spatiotemporal_device")).mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.LECIA);
+    }
 
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack stack = playerIn.getHeldItem(handIn);
+
+        if (!worldIn.isRemote) {
+            for (ServerWorld serverWorld : playerIn.getServer().getWorlds()) {
+                serverWorld.setDayTime(serverWorld.getDayTime() + 6000L);
+            }
+
+            playerIn.getCooldownTracker().setCooldown(stack.getItem(), 12000);
+        }
+
+        return ActionResult.func_233538_a_(stack, worldIn.isRemote);
+    }
 }
