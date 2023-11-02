e61d4beb15dd217599ebdcdb494ab09a61237837
17146 <1714673995@qq.com>
2023-11-03 02:28:18 +0800

    添加了蒜香西瓜和纱Kana的材质

--- src/main/java/tech/lq0/providencraft/item/providencefourth/sana/Shakana.java
+++ src/main/java/tech/lq0/providencraft/item/providencefourth/sana/Shakana.java
@@ -1,11 +1,25 @@
 package tech.lq0.providencraft.item.providencefourth.sana;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import tech.lq0.providencraft.group.ModGroup;
 import tech.lq0.providencraft.init.EffectRegistry;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class Shakana extends Item {
     private static final Food food = (new Food.Builder()).setAlwaysEdible().hunger(2).saturation(0.25f).fastToEat()
@@ -47,6 +61,14 @@ public class Shakana extends Item {
             .build();
 
     public Shakana() {
-        super(new Item.Properties().group(ModGroup.itemgroup).food(food));
+        super(new Item.Properties().group(ModGroup.itemgroup).food(food).rarity(Rarity.UNCOMMON));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("des.providencraft.shakana_1").mergeStyle(TextFormatting.GRAY));
+        tooltip.add(new TranslationTextComponent("des.providencraft.shakana_2").mergeStyle(TextFormatting.GRAY));
+        TooltipTool.addLiverInfo(tooltip, Livers.SANA);
     }
 }
