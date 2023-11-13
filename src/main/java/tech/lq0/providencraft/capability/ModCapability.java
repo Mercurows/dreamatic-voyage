5c00b83e44391b2a333584e98656119f86d584dd
Light_Quanta <zch22248@foxmail.com>
2023-11-14 00:24:27 +0800

    更新版本至1.20

--- src/main/java/tech/lq0/providencraft/capability/ModCapability.java
+++
@@ -1,9 +0,0 @@
-package tech.lq0.providencraft.capability;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityInject;
-
-public class ModCapability {
-    @CapabilityInject(IChaosCapability.class)
-    public static Capability<IChaosCapability> CHAOS_CAPABILITY;
-}
