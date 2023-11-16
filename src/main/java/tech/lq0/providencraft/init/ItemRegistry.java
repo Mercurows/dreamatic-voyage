2cb79fe8786061cab77cc27b0af0e8f4a4e7862a
17146 <1714673995@qq.com>
2023-11-17 00:08:35 +0800

    移植了妖饭团

--- src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
+++ src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
@@ -13,7 +13,9 @@ import tech.lq0.providencraft.item.providenceOI.shirako.MomoDaifuku;
 import tech.lq0.providencraft.item.providencefirst.haya.BigSpoon;
 import tech.lq0.providencraft.item.providencefirst.madoka.RedAhoge;
 import tech.lq0.providencraft.item.providencefirst.madoka.RedAhogeSword;
+import tech.lq0.providencraft.item.providencefirst.unia.EnchantedUnigiri;
 import tech.lq0.providencraft.item.providencefirst.unia.TaroIceCream;
+import tech.lq0.providencraft.item.providencefirst.unia.Unigiri;
 import tech.lq0.providencraft.item.providencefourth.choco.ChocoalCookie;
 import tech.lq0.providencraft.item.providencefourth.choco.StrawberryCake;
 import tech.lq0.providencraft.item.providencefourth.sana.Shakana;
@@ -79,8 +81,8 @@ public class ItemRegistry {
 
     //unia
     public static final RegistryObject<Item> TARO_ICE_CREAM = ITEMS.register("taro_ice_cream", TaroIceCream::new);
-//    public static final RegistryObject<Item> UNIGIRI = ITEMS.register("unigiri", Unigiri::new);
-//    public static final RegistryObject<Item> ENCHANTED_UNIGIRI = ITEMS.register("enchanted_unigiri", EnchantedUnigiri::new);
+    public static final RegistryObject<Item> UNIGIRI = ITEMS.register("unigiri", Unigiri::new);
+    public static final RegistryObject<Item> ENCHANTED_UNIGIRI = ITEMS.register("enchanted_unigiri", EnchantedUnigiri::new);
 //    public static final RegistryObject<Item> ELF_WINGS = ITEMS.register("elf_wings", ElfWings::new);
 
     /**
---
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/unia/EnchantedUnigiri.java
@@ -0,0 +1,38 @@
+package tech.lq0.providencraft.item.providencefirst.unia;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import java.util.List;
+
+public class EnchantedUnigiri extends Item {
+    private static final FoodProperties food = (new FoodProperties.Builder()).saturationMod(0.5f).nutrition(14).
+            effect(() -> new MobEffectInstance(MobEffects.JUMP, 140, 0), 1.0F).
+            effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 140, 0), 1.0F).build();
+
+    public EnchantedUnigiri() {
+        super(new Properties().food(food).rarity(Rarity.UNCOMMON));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.providencraft.unigiri_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.providencraft.enchanted_unigiri").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(pTooltipComponents, Livers.UNIA);
+    }
+}
