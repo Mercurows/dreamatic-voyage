7fd2a8819b815ee8c1540b23c7e9fb73bc8d4698
17146 <1714673995@qq.com>
2023-12-02 21:33:04 +0800

    移植了日程表

--- src/main/java/tech/lq0/providencraft/item/providencefirst/myanna/Schedule.java
+++ src/main/java/tech/lq0/providencraft/item/providencefirst/myanna/Schedule.java
@@ -1,9 +1,50 @@
 package tech.lq0.providencraft.item.providencefirst.myanna;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+import tech.lq0.providencraft.init.SoundRegistry;
+import tech.lq0.providencraft.tools.Livers;
+import tech.lq0.providencraft.tools.TooltipTool;
+
+import java.util.List;
 
 public class Schedule extends Item {
     public Schedule() {
-        super(new Properties());
+        super(new Properties().durability(7).setNoRepair());
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.providencraft.schedule_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.providencraft.schedule_2").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(pTooltipComponents, Livers.MYANNA);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+
+        if (!pLevel.isClientSide) {
+            pPlayer.addEffect(new MobEffectInstance(MobEffects.DIG_SPEED, 1200, 0));
+        }
+        pPlayer.getCooldowns().addCooldown(stack.getItem(), 1800);
+        pLevel.playSound(pPlayer, pPlayer.getOnPos(), SoundRegistry.BABY_MEOW.get(), SoundSource.AMBIENT, 0.65f, 1f);
+
+        return InteractionResultHolder.success(stack);
     }
 }
