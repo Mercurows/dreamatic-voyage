d7084f05d570c287f9fe8b88bf55e17664cd9a0c
17146 <1714673995@qq.com>
2023-12-03 03:26:19 +0800

    添加了新的描述

--- src/main/java/tech/lq0/providencraft/item/material/DarkCrystal.java
+++ src/main/java/tech/lq0/providencraft/item/material/DarkCrystal.java
@@ -1,10 +1,26 @@
 package tech.lq0.providencraft.item.material;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
 import tech.lq0.providencraft.tools.RarityTool;
+
+import java.util.List;
 
 public class DarkCrystal extends Item {
     public DarkCrystal() {
         super(new Properties().rarity(RarityTool.DEEP_DARK));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.providencraft.dark_crystal").withStyle(ChatFormatting.GRAY));
     }
 }
--- src/main/java/tech/lq0/providencraft/item/material/IllusoryCrystal.java
+++ src/main/java/tech/lq0/providencraft/item/material/IllusoryCrystal.java
@@ -1,10 +1,26 @@
 package tech.lq0.providencraft.item.material;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class IllusoryCrystal extends Item {
     public IllusoryCrystal() {
         super(new Properties().rarity(Rarity.RARE));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.providencraft.illusory_crystal").withStyle(ChatFormatting.GRAY));
     }
 }
