c8fece83a50233f5dfffe15cf8d4420659fe4818
17146 <1714673995@qq.com>
2023-12-07 01:51:22 +0800

    移植了混沌检测仪

---
+++ src/main/java/tech/lq0/providencraft/item/other/ChaosChecker.java
@@ -0,0 +1,59 @@
+package tech.lq0.providencraft.item.other;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+import tech.lq0.providencraft.capability.chaos.ChaosHelper;
+import tech.lq0.providencraft.tools.ItemNBTTool;
+
+import java.util.List;
+
+public class ChaosChecker extends Item {
+    public ChaosChecker() {
+        super(new Properties().stacksTo(1));
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        if (!pLevel.isClientSide && pUsedHand == InteractionHand.MAIN_HAND) {
+            int chaos = ChaosHelper.getChaos(pPlayer);
+            pPlayer.displayClientMessage(Component.translatable("des.providencraft.chaos_checker.info", chaos), true);
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.providencraft.chaos_checker_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.providencraft.chaos_checker_2").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
+    }
+
+    @Override
+    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
+        if (pEntity instanceof Player player && !pLevel.isClientSide) {
+            int chaos = ChaosHelper.getChaos(player);
+
+            ItemNBTTool.setInt(pStack, "chaos", chaos);
+        }
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return slotChanged && !oldStack.equals(newStack);
+    }
+
+    public static int getChaos(ItemStack stack) {
+        return ItemNBTTool.getInt(stack, "chaos", 0);
+    }
+}
