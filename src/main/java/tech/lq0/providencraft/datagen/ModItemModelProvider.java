3cc51a43abf09efde5e2cca9cf4e6f7894c6cc4a
17146 <1714673995@qq.com>
2023-12-11 01:40:12 +0800

    移植部分物品模型

--- src/main/java/tech/lq0/providencraft/datagen/ModItemModelProvider.java
+++ src/main/java/tech/lq0/providencraft/datagen/ModItemModelProvider.java
@@ -21,6 +21,7 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
+        //1st
         simpleItem(ItemRegistry.RED_AHOGE);
         handheldItem(ItemRegistry.RED_AHOGE_SWORD);
         handheldItem(ItemRegistry.RED_AHOGE_BOOMERANG);
@@ -46,6 +47,7 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ItemRegistry.ENCHANTED_UNIGIRI);
         simpleItem(ItemRegistry.ELF_WINGS);
 
+        //2nd
         simpleItem(ItemRegistry.LUCIA_RICE_CAKE);
         simpleItem(ItemRegistry.BIG_MOE_ONE_POTION);
         simpleItem(ItemRegistry.MOONLIGHT_AMULET);
@@ -64,9 +66,91 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ItemRegistry.MECHANICAL_HORN);
 
         handheldItem(ItemRegistry.SATOU_KNIFE);
+        simpleItem(ItemRegistry.ISEKAI_LOLLIPOP);
+        simpleItem(ItemRegistry.PAST_SUGAR);
         simpleItem(ItemRegistry.SATOU_CHESTPLATE);
 
+        simpleItem(ItemRegistry.AYELET_MASK);
+        simpleItem(ItemRegistry.MUMU_STEAK);
 
+        //mc
+        simpleItem(ItemRegistry.FLAT_VEGETABLE_CHESTPLATE);
+        simpleItem(ItemRegistry.STEEL_PLATE);
+        simpleItem(ItemRegistry.LOTUS_POTATO);
+//        simpleItem(ItemRegistry.CHIRAM_LANTERN);
+        simpleItem(ItemRegistry.BREEZE_CROWN);
+        simpleItem(ItemRegistry.CHIRAM_CORE);
+//        simpleItem(ItemRegistry.MAGIC_TEAPOT);
+
+        simpleItem(ItemRegistry.MARSHAIMALLOW);
+        simpleItem(ItemRegistry.CLOUD_KEY);
+        simpleItem(ItemRegistry.CURSED_MARSHAIMALLOW);
+        simpleItem(ItemRegistry.MISTY_CHESTPLATE);
+        simpleItem(ItemRegistry.HAINE_CORE);
+//        simpleItem(ItemRegistry.HAIBARA_SOUP);
+
+
+        //3rd
+
+
+        //oi
+
+
+        //4th
+
+
+        //cnt
+
+
+        //others
+        simpleItem(ItemRegistry.MMA_STEW);
+        simpleItem(ItemRegistry.MVA_STEW);
+        simpleItem(ItemRegistry.PDC_POINT);
+        simpleItem(ItemRegistry.PREMIUM_PDC_POINT);
+        simpleItem(ItemRegistry.CHAOS_CHECKER);
+        simpleItem(ItemRegistry.ESCORT_TOKEN);
+
+        //legacy
+        simpleItem(ItemRegistry.YAGI_STAFF_CARD);
+        simpleItem(ItemRegistry.LAVA_CAKE);
+
+
+        //material
+        simpleItem(ItemRegistry.IDOL_CLOTH);
+        simpleItem(ItemRegistry.GN_SOUL);
+        simpleItem(ItemRegistry.GN_SPIRIT);
+        simpleItem(ItemRegistry.GN_STAR);
+        simpleItem(ItemRegistry.MAGICROS_CORE);
+        simpleItem(ItemRegistry.DREAM_WEAVING_YARN);
+        simpleItem(ItemRegistry.DARK_CRYSTAL);
+        simpleItem(ItemRegistry.RADIANT_CRYSTAL);
+        simpleItem(ItemRegistry.ILLUSORY_CRYSTAL);
+        simpleItem(ItemRegistry.SINK_CRYSTAL);
+        simpleItem(ItemRegistry.ALIENIS_CRYSTAL);
+        simpleItem(ItemRegistry.TERMINAL_CRYSTAL);
+
+        //audio
+
+
+        //donate
+
+
+        //costume
+        simpleItem(ItemRegistry.CHIRAM_IDOL_HELMET);
+        simpleItem(ItemRegistry.CHIRAM_IDOL_CHESTPLATE);
+        simpleItem(ItemRegistry.CHIRAM_IDOL_BOOTS);
+        simpleItem(ItemRegistry.HAINE_IDOL_HELMET);
+        simpleItem(ItemRegistry.HAINE_IDOL_CHESTPLATE);
+        simpleItem(ItemRegistry.HAINE_IDOL_BOOTS);
+        simpleItem(ItemRegistry.KERORO_IDOL_HELMET);
+        simpleItem(ItemRegistry.KERORO_IDOL_CHESTPLATE);
+        simpleItem(ItemRegistry.KERORO_IDOL_BOOTS);
+        simpleItem(ItemRegistry.EKIRA_IDOL_HELMET);
+        simpleItem(ItemRegistry.EKIRA_IDOL_CHESTPLATE);
+        simpleItem(ItemRegistry.EKIRA_IDOL_BOOTS);
+
+
+        //blocks
         evenSimplerBlockItem(BlockRegistry.PORCELAIN_THRONE);
         evenSimplerBlockItem(BlockRegistry.BLUE_SANDSTONE_SLAB);
         evenSimplerBlockItem(BlockRegistry.SMOOTH_BLUE_SANDSTONE_SLAB);
@@ -76,13 +160,13 @@ public class ModItemModelProvider extends ItemModelProvider {
     private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(Utils.MOD_ID,"block/" + item.getId().getPath()));
+                new ResourceLocation(Utils.MOD_ID, "block/" + item.getId().getPath()));
     }
 
     private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(Utils.MOD_ID,"item/" + item.getId().getPath()));
+                new ResourceLocation(Utils.MOD_ID, "item/" + item.getId().getPath()));
     }
 
     public void evenSimplerBlockItem(RegistryObject<Block> block) {
@@ -97,34 +181,34 @@ public class ModItemModelProvider extends ItemModelProvider {
 
     public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
-                .texture("texture",  new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+                .texture("texture", new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
     }
 
     public void buttonItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
-                .texture("texture",  new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+                .texture("texture", new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
     }
 
     public void wallItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
         this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/wall_inventory"))
-                .texture("wall",  new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+                .texture("wall", new ResourceLocation(Utils.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
     }
 
     private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/handheld")).texture("layer0",
-                new ResourceLocation(Utils.MOD_ID,"item/" + item.getId().getPath()));
+                new ResourceLocation(Utils.MOD_ID, "item/" + item.getId().getPath()));
     }
 
     private ItemModelBuilder simpleBlockItem(RegistryObject<Block> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(Utils.MOD_ID,"item/" + item.getId().getPath()));
+                new ResourceLocation(Utils.MOD_ID, "item/" + item.getId().getPath()));
     }
 
     private ItemModelBuilder simpleBlockItemBlockTexture(RegistryObject<Block> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(Utils.MOD_ID,"block/" + item.getId().getPath()));
+                new ResourceLocation(Utils.MOD_ID, "block/" + item.getId().getPath()));
     }
 }
