47423ac019e4d3aae6a3ca44b82e813738733ec5
17146 <1714673995@qq.com>
2023-12-11 01:26:53 +0800

    移植部分物品模型

--- src/main/java/tech/lq0/providencraft/datagen/ModItemModelProvider.java
+++ src/main/java/tech/lq0/providencraft/datagen/ModItemModelProvider.java
@@ -10,6 +10,7 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import tech.lq0.providencraft.Utils;
+import tech.lq0.providencraft.init.BlockRegistry;
 import tech.lq0.providencraft.init.ItemRegistry;
 
 @SuppressWarnings({"ConstantConditions", "UnusedReturnValue"})
@@ -45,6 +46,31 @@ public class ModItemModelProvider extends ItemModelProvider {
         simpleItem(ItemRegistry.ENCHANTED_UNIGIRI);
         simpleItem(ItemRegistry.ELF_WINGS);
 
+        simpleItem(ItemRegistry.LUCIA_RICE_CAKE);
+        simpleItem(ItemRegistry.BIG_MOE_ONE_POTION);
+        simpleItem(ItemRegistry.MOONLIGHT_AMULET);
+        simpleItem(ItemRegistry.BIG_FIERCE_ONE_COFFEE);
+
+        simpleItem(ItemRegistry.WHITE_AHOGE);
+        simpleItem(ItemRegistry.WHITE_DOUBLE_AHOGE);
+        simpleItem(ItemRegistry.MAGIC_BOOK);
+        simpleItem(ItemRegistry.MAGIC_BOOK_MYANNA);
+        simpleItem(ItemRegistry.MAGIC_BOOK_MYANNA_PLUS);
+        simpleItem(ItemRegistry.MAGIC_BOOK_THROW);
+        simpleItem(ItemRegistry.MAGIC_BOOK_REVELATION);
+        simpleItem(ItemRegistry.LOUISTEW);
+        simpleItem(ItemRegistry.UNI_MILLET);
+
+        simpleItem(ItemRegistry.MECHANICAL_HORN);
+
+        handheldItem(ItemRegistry.SATOU_KNIFE);
+        simpleItem(ItemRegistry.SATOU_CHESTPLATE);
+
+
+        evenSimplerBlockItem(BlockRegistry.PORCELAIN_THRONE);
+        evenSimplerBlockItem(BlockRegistry.BLUE_SANDSTONE_SLAB);
+        evenSimplerBlockItem(BlockRegistry.SMOOTH_BLUE_SANDSTONE_SLAB);
+        evenSimplerBlockItem(BlockRegistry.CUT_BLUE_SANDSTONE_SLAB);
     }
 
     private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
