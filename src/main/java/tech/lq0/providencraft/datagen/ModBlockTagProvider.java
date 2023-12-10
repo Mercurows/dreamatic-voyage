b6414d0df14d8fb0461a9c613fad00d3d8aa6979
17146 <1714673995@qq.com>
2023-12-11 03:03:24 +0800

    添加了部分方块标签

--- src/main/java/tech/lq0/providencraft/datagen/ModBlockTagProvider.java
+++ src/main/java/tech/lq0/providencraft/datagen/ModBlockTagProvider.java
@@ -31,23 +31,37 @@ public class ModBlockTagProvider extends BlockTagsProvider {
 
         this.tag(BlockTags.MINEABLE_WITH_SHOVEL).add(BlockRegistry.BLUE_SAND.get());
 
-        this.tag(BlockTags.MINEABLE_WITH_AXE)
-                .add(BlockRegistry.UNI_MILLET_BLOCK.get(), BlockRegistry.HARANO_LOG.get());
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(BlockRegistry.UNI_MILLET_BLOCK.get(), BlockRegistry.HARANO_LOG.get());
 
-        this.tag(BlockTags.MINEABLE_WITH_HOE)
-                .add(BlockRegistry.HARANO_LEAVES.get());
+        this.tag(BlockTags.MINEABLE_WITH_HOE).add(BlockRegistry.HARANO_LEAVES.get(), BlockRegistry.UNI_MILLET_BLOCK.get());
 
-        this.tag(BlockTags.BEE_GROWABLES)
-                .add(BlockRegistry.UNI_MILLET_BLOCK.get());
+        this.tag(BlockTags.BEE_GROWABLES).add(BlockRegistry.UNI_MILLET_BLOCK.get());
 
-        this.tag(BlockTags.CROPS)
-                .add(BlockRegistry.UNI_MILLET_BLOCK.get());
+        this.tag(BlockTags.CROPS).add(BlockRegistry.UNI_MILLET_BLOCK.get());
 
-        this.tag(BlockTags.SAND)
-                .add(BlockRegistry.BLUE_SAND.get());
+        this.tag(BlockTags.SAND).add(BlockRegistry.BLUE_SAND.get());
 
         this.tag(Tags.Blocks.SANDSTONE)
                 .add(BlockRegistry.BLUE_SANDSTONE.get(), BlockRegistry.CUT_BLUE_SANDSTONE.get(),
                         BlockRegistry.SMOOTH_BLUE_SANDSTONE.get(), BlockRegistry.CHISELED_BLUE_SANDSTONE.get());
+
+        this.tag(BlockTags.LOGS)
+                .add(BlockRegistry.HARANO_LOG.get());
+
+        this.tag(BlockTags.LOGS_THAT_BURN)
+                .add(BlockRegistry.HARANO_LOG.get());
+
+        this.tag(BlockTags.WALLS).add(BlockRegistry.BLUE_SANDSTONE_WALL.get());
+
+        this.tag(BlockTags.SWORD_EFFICIENT).add(BlockRegistry.UNI_MILLET_BLOCK.get());
+
+        this.tag(BlockTags.SLABS)
+                .add(BlockRegistry.BLUE_SANDSTONE_SLAB.get(), BlockRegistry.SMOOTH_BLUE_SANDSTONE_SLAB.get(),
+                        BlockRegistry.CUT_BLUE_SANDSTONE_SLAB.get());
+
+        this.tag(BlockTags.SMELTS_TO_GLASS).add(BlockRegistry.BLUE_SAND.get());
+
+        this.tag(BlockTags.STAIRS)
+                .add(BlockRegistry.BLUE_SANDSTONE_STAIRS.get(), BlockRegistry.SMOOTH_BLUE_SANDSTONE_STAIRS.get());
     }
 }
