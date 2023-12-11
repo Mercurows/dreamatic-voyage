fad6f6bbddfaabcf590ba40426c63e8dfd864577
17146 <1714673995@qq.com>
2023-12-11 20:34:44 +0800

    移植了呆毛大砍刀

--- src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
+++ src/main/java/tech/lq0/providencraft/init/ItemRegistry.java
@@ -86,7 +86,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> RED_AHOGE_BOOMERANG = ITEMS.register("red_ahoge_boomerang", RedAhogeBoomerang::new);
     public static final RegistryObject<Item> RED_AHOGE_HELMET = ITEMS.register("red_ahoge_helmet", RedAhogeHelmet::new);
     public static final RegistryObject<Item> PEACH_BEACH = ITEMS.register("peach_beach", PeachBeach::new);
-    //    public static final RegistryObject<Item> RED_AHOGE_MACHETE = ITEMS.register("red_ahoge_machete", RedAhogeMachete::new);
+    public static final RegistryObject<Item> RED_AHOGE_MACHETE = ITEMS.register("red_ahoge_machete", RedAhogeMachete::new);
     public static final RegistryObject<Item> SNEAKERS = ITEMS.register("sneakers", Sneakers::new);
     public static final RegistryObject<Item> CRIMSON_IMPACT = ITEMS.register("crimson_impact", CrimsonImpact::new);
 
