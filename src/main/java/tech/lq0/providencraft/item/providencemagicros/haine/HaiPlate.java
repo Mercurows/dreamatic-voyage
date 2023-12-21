d91c2b4c7ce5e762e2deebb550f1fd008776aa4b
17146 <1714673995@qq.com>
2023-12-22 04:19:51 +0800

    修复文本问题

--- src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/HaiPlate.java
+++ src/main/java/tech/lq0/providencraft/item/providencemagicros/haine/HaiPlate.java
@@ -53,7 +53,7 @@ public class HaiPlate extends ShieldItem {
         pTooltipComponents.add(Component.translatable("des.providencraft.hai_plate.func").withStyle(ChatFormatting.AQUA));
         pTooltipComponents.add(Component.translatable("des.providencraft.hai_plate_1").withStyle(ChatFormatting.GRAY));
         pTooltipComponents.add(Component.translatable("des.providencraft.hai_plate_2").withStyle(ChatFormatting.GRAY));
-        pTooltipComponents.add(Component.translatable("des.providencraft.hai_plate_2").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
+        pTooltipComponents.add(Component.translatable("des.providencraft.hai_plate_3").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
 
         TooltipTool.addLiverInfo(pTooltipComponents, Livers.HAINE);
     }
