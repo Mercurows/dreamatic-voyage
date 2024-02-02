15de1d98637b15ea827192d9d69a23cf276df6cf
17146 <1714673995@qq.com>
2024-02-03 01:27:16 +0800

    添加了新附魔

--- src/main/java/tech/lq0/providencraft/init/EnchantmentRegistry.java
+++ src/main/java/tech/lq0/providencraft/init/EnchantmentRegistry.java
@@ -5,10 +5,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import tech.lq0.providencraft.Utils;
-import tech.lq0.providencraft.enchantment.AhogeParasitic;
-import tech.lq0.providencraft.enchantment.Magicros;
-import tech.lq0.providencraft.enchantment.PeaceWish;
-import tech.lq0.providencraft.enchantment.UniHusk;
+import tech.lq0.providencraft.enchantment.*;
 
 public class EnchantmentRegistry {
     public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Utils.MOD_ID);
@@ -17,4 +14,5 @@ public class EnchantmentRegistry {
     public static final RegistryObject<Enchantment> UNI_HUSK = ENCHANTMENTS.register("uni_husk", UniHusk::new);
     public static final RegistryObject<Enchantment> MAGICROS = ENCHANTMENTS.register("magicros", Magicros::new);
     public static final RegistryObject<Enchantment> PEACE_WISH = ENCHANTMENTS.register("peace_wish", PeaceWish::new);
+    public static final RegistryObject<Enchantment> FAIR_MEANS = ENCHANTMENTS.register("fair_means", FairMeans::new);
 }
