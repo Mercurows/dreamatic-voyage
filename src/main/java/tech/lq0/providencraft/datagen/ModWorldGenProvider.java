92b48c3dc4c9136e3be5b554fc52a5ece170d891
17146 <1714673995@qq.com>
2024-02-07 15:23:00 +0800

    更新生成项

--- src/main/java/tech/lq0/providencraft/datagen/ModWorldGenProvider.java
+++ src/main/java/tech/lq0/providencraft/datagen/ModWorldGenProvider.java
@@ -7,13 +7,15 @@ import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import tech.lq0.providencraft.Utils;
 import tech.lq0.providencraft.worldgen.ModConfiguredFeatures;
+import tech.lq0.providencraft.worldgen.biome.ModBiomes;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.BIOME, ModBiomes::boostrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(Utils.MOD_ID));
