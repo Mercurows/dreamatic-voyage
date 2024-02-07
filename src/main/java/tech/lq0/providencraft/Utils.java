bc28dd6f62013cb9d6c4aa270213a69250f91f3a
17146 <1714673995@qq.com>
2024-02-07 16:57:06 +0800

    添加了流血药水

--- src/main/java/tech/lq0/providencraft/Utils.java
+++ src/main/java/tech/lq0/providencraft/Utils.java
@@ -1,6 +1,13 @@
 package tech.lq0.providencraft;
 
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -30,6 +37,7 @@ public class Utils {
         LootModifierRegistry.register(modEventBus);
         VillagerRegistry.register(modEventBus);
         AttributeRegistry.ATTRIBUTES.register(modEventBus);
+        PotionRegistry.POTIONS.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,7 +46,10 @@ public class Utils {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-
+        event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.SPLASH_POTION), Potions.AWKWARD)),
+                Ingredient.of(Tags.Items.GLASS), PotionUtils.setPotion(new ItemStack(Items.POTION), PotionRegistry.BLEEDING.get())));
+        event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), PotionRegistry.BLEEDING.get())),
+                Ingredient.of(ItemRegistry.PAST_SUGAR.get()), PotionUtils.setPotion(new ItemStack(Items.POTION), PotionRegistry.SUPER_BLEEDING.get())));
     }
 
     private void registerForgeEvents() {
---
+++ src/main/java/tech/lq0/providencraft/init/PotionRegistry.java
@@ -0,0 +1,17 @@
+package tech.lq0.providencraft.init;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import tech.lq0.providencraft.Utils;
+
+public class PotionRegistry {
+    public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTIONS, Utils.MOD_ID);
+
+    public static final RegistryObject<Potion> BLEEDING = POTIONS.register("pdc_bleeding",
+            () -> new Potion(new MobEffectInstance(EffectRegistry.BLEEDING.get(), 1200, 0)));
+    public static final RegistryObject<Potion> SUPER_BLEEDING = POTIONS.register("super_pdc_bleeding",
+            () -> new Potion(new MobEffectInstance(EffectRegistry.BLEEDING.get(), 300, 4)));
+}
