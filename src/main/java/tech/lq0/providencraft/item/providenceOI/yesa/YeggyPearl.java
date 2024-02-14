47f792debb9c93277225771c6e23dc0fe60be699
17146 <1714673995@qq.com>
2024-02-14 22:49:10 +0800

    添加伊头珠配方

--- src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
+++ src/main/java/tech/lq0/providencraft/item/providenceOI/yesa/YeggyPearl.java
@@ -12,6 +12,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
@@ -36,7 +37,7 @@ public class YeggyPearl extends Item implements ICurioItem {
     public static final String TAG_YEGGY_PEARL = "yeggy_energy";
 
     public YeggyPearl() {
-        super(new Properties().stacksTo(1));
+        super(new Properties().stacksTo(1).rarity(Rarity.UNCOMMON));
     }
 
     @Override
