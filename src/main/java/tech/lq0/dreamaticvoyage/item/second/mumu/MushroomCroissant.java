671ff2cd1bb266cd254c166a6ec94f6cd67ee20f
17146 <1714673995@qq.com>
2024-03-05 13:00:09 +0800

    添加蘑菇牛角包的文案

--- src/main/java/tech/lq0/dreamaticvoyage/item/second/mumu/MushroomCroissant.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/second/mumu/MushroomCroissant.java
@@ -1,6 +1,8 @@
 package tech.lq0.dreamaticvoyage.item.second.mumu;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -9,13 +11,31 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Cow;
 import net.minecraft.world.entity.animal.MushroomCow;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.tools.Livers;
+import tech.lq0.dreamaticvoyage.tools.TooltipTool;
+
+import java.util.List;
 
 public class MushroomCroissant extends Item {
+    private static final FoodProperties food = (new FoodProperties.Builder()).saturationMod(0.8f).nutrition(5).build();
+
     public MushroomCroissant() {
-        super(new Properties().rarity(Rarity.UNCOMMON));
+        super(new Properties().rarity(Rarity.UNCOMMON).food(food));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.mushroom_croissant_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.mushroom_croissant_2").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(pTooltipComponents, Livers.MUMU);
     }
 
     @Override
