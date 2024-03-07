d4b5eccb7991883dae7cc0bd74e5362c72099456
17146 <1714673995@qq.com>
2024-03-07 18:17:36 +0800

    添加了恶兆的文本，调整贴图

--- src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousSickle.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousSickle.java
@@ -1,9 +1,18 @@
 package tech.lq0.dreamaticvoyage.item.fourth.hifumi;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.tiers.ModItemTier;
+import tech.lq0.dreamaticvoyage.tools.Livers;
 import tech.lq0.dreamaticvoyage.tools.RarityTool;
+import tech.lq0.dreamaticvoyage.tools.TooltipTool;
+
+import java.util.List;
 
 public class OminousSickle extends SwordItem {
     public OminousSickle() {
@@ -14,5 +23,13 @@ public class OminousSickle extends SwordItem {
     @Override
     public boolean isDamageable(ItemStack stack) {
         return false;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.ominous_sickle_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.ominous_sickle_2").withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
+
+        TooltipTool.addLiverInfo(pTooltipComponents, Livers.HIFUMI);
     }
 }
