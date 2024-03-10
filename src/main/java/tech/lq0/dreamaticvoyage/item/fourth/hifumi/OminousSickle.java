99e6def3ddc0c9ac0d5f3ed2f4e35deb9a414334
17146 <1714673995@qq.com>
2024-03-11 01:29:12 +0800

    添加了恶兆的部分功能

--- src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousSickle.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousSickle.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,7 +17,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.CriticalHitEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.Utils;
+import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.tiers.ModItemTier;
 import tech.lq0.dreamaticvoyage.tools.ItemNBTTool;
 import tech.lq0.dreamaticvoyage.tools.Livers;
@@ -23,6 +29,8 @@ import tech.lq0.dreamaticvoyage.tools.RarityTool;
 import tech.lq0.dreamaticvoyage.tools.TooltipTool;
 
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class OminousSickle extends SwordItem {
     public static final String TAG_HEALTH = "Health";
@@ -38,14 +46,27 @@ public class OminousSickle extends SwordItem {
                 (int) (12 / ItemNBTTool.getFloat(stack, TAG_HEALTH, 1.0f)) - 1, AttributeModifier.Operation.ADDITION));
         builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier",
                 -2 + (1 - ItemNBTTool.getFloat(stack, TAG_HEALTH, 1.0f)) / 0.8f, AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(new UUID(ItemRegistry.OMINOUS_SICKLE.hashCode(), 0), Utils.MOD_ATTRIBUTE_MODIFIER,
+                0.2, AttributeModifier.Operation.MULTIPLY_BASE));
         return builder.build();
     }
 
     @Override
     public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        pAttacker.heal((1 - ItemNBTTool.getFloat(pStack, TAG_HEALTH, 1.0f)) / 8f * pAttacker.getMaxHealth());
+        Random random = new Random();
+        if (random.nextFloat() < .3f) {
+            pTarget.addEffect(new MobEffectInstance(MobEffects.UNLUCK, 200, 0));
+        }
 
-        return super.hurtEnemy(pStack, pTarget, pAttacker);
+        float healAmount = (1 - ItemNBTTool.getFloat(pStack, TAG_HEALTH, 1.0f)) / 8f * pAttacker.getMaxHealth();
+
+        if (pTarget.hasEffect(MobEffects.UNLUCK)) {
+            pAttacker.heal(healAmount * 2f);
+        } else {
+            pAttacker.heal(healAmount);
+        }
+
+        return true;
     }
 
     @Override
@@ -79,5 +100,16 @@ public class OminousSickle extends SwordItem {
     @Override
     public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
         return slotChanged;
+    }
+
+    @SubscribeEvent
+    public static void onCriticalHit(CriticalHitEvent event) {
+        Player player = event.getEntity();
+        if (event.getTarget() instanceof LivingEntity living && living.hasEffect(MobEffects.UNLUCK)) {
+            ItemStack stack = player.getMainHandItem();
+            if (stack.getItem() == ItemRegistry.OMINOUS_SICKLE.get()) {
+                event.setDamageModifier(event.getDamageModifier() * 2f);
+            }
+        }
     }
 }
