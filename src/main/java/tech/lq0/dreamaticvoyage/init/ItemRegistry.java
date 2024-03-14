772c3d3ed2b77e6e7a0debfb3281235f08778023
17146 <1714673995@qq.com>
2024-03-14 23:00:49 +0800

    注册了虚实熔铸符

--- src/main/java/tech/lq0/dreamaticvoyage/init/ItemRegistry.java
+++ src/main/java/tech/lq0/dreamaticvoyage/init/ItemRegistry.java
@@ -361,6 +361,7 @@ public class ItemRegistry {
     public static final RegistryObject<Item> FALLEN_RING = ITEMS.register("fallen_ring", FallenRing::new);
     public static final RegistryObject<Item> ESCORT_TOKEN = ITEMS.register("escort_token", EscortToken::new);
     public static final RegistryObject<Item> ANCIENT_MICROPHONE = ITEMS.register("ancient_microphone", AncientMicrophone::new);
+    public static final RegistryObject<Item> FICTIONAL_FORGING_SIGIL = ITEMS.register("fictional_forging_sigil", FictionalForgingSigil::new);
 
     /**
      * legacy
