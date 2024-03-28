00df366c0d1ae9f2d7fe6f4bedb1cf9bfd5a714a
17146 <1714673995@qq.com>
2024-03-28 20:38:47 +0800

    添加甜蜜幻象的功能

--- src/main/java/tech/lq0/dreamaticvoyage/effect/SweetMirage.java
+++ src/main/java/tech/lq0/dreamaticvoyage/effect/SweetMirage.java
@@ -1,10 +1,35 @@
 package tech.lq0.dreamaticvoyage.effect;
 
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import tech.lq0.dreamaticvoyage.init.EffectRegistry;
 
+import java.util.Random;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class SweetMirage extends MobEffect {
     public SweetMirage() {
         super(MobEffectCategory.BENEFICIAL, 0xA98880);
+    }
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+        DamageSource source = event.getSource();
+        LivingEntity living = event.getEntity();
+        if (!source.is(DamageTypeTags.BYPASSES_ARMOR) && living.hasEffect(EffectRegistry.SWEET_MIRAGE.get())) {
+            Random random = new Random();
+            int level = living.getEffect(EffectRegistry.SWEET_MIRAGE.get()).getAmplifier() + 1;
+            double chance = -9.0 / (0.5 * (double)level + 1.0) + 9.0;
+            if (random.nextDouble() < chance / 10.0) {
+                event.setAmount(0);
+                living.heal(level * 2);
+            }
+        }
     }
 }
