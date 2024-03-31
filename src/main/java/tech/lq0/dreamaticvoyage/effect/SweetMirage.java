618e3cf204fcf78e7e18531658a4986483d4b97c
17146 <1714673995@qq.com>
2024-04-01 03:01:50 +0800

    添加了美食家证章的功能

--- src/main/java/tech/lq0/dreamaticvoyage/effect/SweetMirage.java
+++ src/main/java/tech/lq0/dreamaticvoyage/effect/SweetMirage.java
@@ -4,6 +4,7 @@ import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -24,11 +25,15 @@ public class SweetMirage extends MobEffect {
         LivingEntity living = event.getEntity();
         if (!source.is(DamageTypeTags.BYPASSES_ARMOR) && living.hasEffect(EffectRegistry.SWEET_MIRAGE.get())) {
             Random random = new Random();
-            int level = living.getEffect(EffectRegistry.SWEET_MIRAGE.get()).getAmplifier() + 1;
-            double chance = -9.0 / (0.5 * (double)level + 1.0) + 9.0;
-            if (random.nextDouble() < chance / 10.0) {
-                event.setAmount(0);
-                living.heal(level * 2);
+
+            MobEffectInstance instance = living.getEffect(EffectRegistry.SWEET_MIRAGE.get());
+            if (instance != null) {
+                int level = instance.getAmplifier() + 1;
+                double chance = -9.0 / (0.5 * (double) level + 1.0) + 9.0;
+                if (random.nextDouble() < chance / 10.0) {
+                    event.setAmount(0);
+                    living.heal(level * 2);
+                }
             }
         }
     }
--- src/main/java/tech/lq0/dreamaticvoyage/item/other/GourmetMedal.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/other/GourmetMedal.java
@@ -4,7 +4,11 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -13,11 +17,29 @@ import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.Utils;
 import tech.lq0.dreamaticvoyage.tools.ItemNBTTool;
 import tech.lq0.dreamaticvoyage.tools.RarityTool;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GourmetMedal extends Item implements ICurioItem {
+    private static final List<MobEffect> EFFECTS = new ArrayList<>();
+
+    static {
+        EFFECTS.add(MobEffects.MOVEMENT_SLOWDOWN);
+        EFFECTS.add(MobEffects.DIG_SLOWDOWN);
+        EFFECTS.add(MobEffects.CONFUSION);
+        EFFECTS.add(MobEffects.HUNGER);
+        EFFECTS.add(MobEffects.POISON);
+        EFFECTS.add(MobEffects.UNLUCK);
+        EFFECTS.add(MobEffects.WITHER);
+        EFFECTS.add(MobEffects.WEAKNESS);
+        EFFECTS.add(MobEffects.BLINDNESS);
+    }
+
     public static final String TAG_GOURMET_MEDAL = "GourmetMedal";
 
     public GourmetMedal() {
@@ -32,8 +54,8 @@ public class GourmetMedal extends Item implements ICurioItem {
         if (progress >= 1f) {
             pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.gourmet_medal.done").withStyle(ChatFormatting.GOLD).withStyle(ChatFormatting.ITALIC));
         } else {
-            pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.gourmet_medal.need").withStyle(ChatFormatting.RED)
-                    .append(Component.literal(String.format("%.2f%%", progress * 100))));
+            pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.gourmet_medal.need").withStyle(ChatFormatting.WHITE)
+                    .append(Component.literal(String.format(" %.2f%%", progress * 100))));
         }
     }
 
@@ -47,5 +69,46 @@ public class GourmetMedal extends Item implements ICurioItem {
             }
         }
         super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
+    }
+
+    @Override
+    public boolean canEquip(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        AtomicBoolean flag = new AtomicBoolean(true);
+        CuriosApi.getCuriosInventory(livingEntity).ifPresent(c -> c.findFirstCurio(this).ifPresent(s -> flag.set(false)));
+
+        return flag.get();
+    }
+
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity entity = slotContext.entity();
+
+        float progress = ItemNBTTool.getFloat(stack, TAG_GOURMET_MEDAL, 0f);
+
+        for (MobEffect mobEffect : EFFECTS) {
+            if (entity != null && entity.hasEffect(mobEffect)) {
+                MobEffectInstance effectInstance = entity.getEffect(mobEffect);
+                if (effectInstance != null) {
+                    int level = effectInstance.getAmplifier();
+
+                    if (progress >= 1f) {
+                        if (level <= 2) {
+                            entity.removeEffect(mobEffect);
+                        }
+                    } else if (progress >= 0.75f) {
+                        if (level <= 1) {
+                            entity.removeEffect(mobEffect);
+                        }
+                    } else if (progress >= 0.5f) {
+                        if (level <= 0) {
+                            entity.removeEffect(mobEffect);
+                        }
+                    }
+                }
+            }
+        }
+
+        ICurioItem.super.curioTick(slotContext, stack);
     }
 }
