275be5870465d162e8bda6266fbc8ecdfda712dc
17146 <1714673995@qq.com>
2024-04-01 04:56:52 +0800

    添加了厄运之骨的获取方式

--- src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousBone.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/fourth/hifumi/OminousBone.java
@@ -12,18 +12,26 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.WitherSkeleton;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.Utils;
+import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.tools.Livers;
 import tech.lq0.dreamaticvoyage.tools.TooltipTool;
 
 import java.util.List;
+import java.util.Random;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class OminousBone extends Item {
     public OminousBone() {
         super(new Properties().rarity(Rarity.UNCOMMON).fireResistant());
@@ -54,5 +62,35 @@ public class OminousBone extends Item {
                     new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, Utils.MOD_ATTRIBUTE_MODIFIER, 4.0, AttributeModifier.Operation.ADDITION));
         }
         return map;
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof WitherSkeleton skeleton && !skeleton.level().isClientSide) {
+            if (event.getSource().getEntity() instanceof Player player) {
+                if (player.hasEffect(MobEffects.BAD_OMEN)) {
+                    MobEffectInstance instance = player.getEffect(MobEffects.BAD_OMEN);
+                    if (instance != null) {
+                        int level = instance.getAmplifier();
+
+                        if (level >= 4) {
+                            Random random = new Random();
+                            double luck = player.getAttributeValue(Attributes.LUCK);
+
+                            double chance = Math.min(.2, Math.max(.005, .025 * (1 + luck * .2)));
+                            if (random.nextDouble(0, 1) < chance) {
+                                ItemStack stack = new ItemStack(ItemRegistry.OMINOUS_BONE.get());
+
+                                skeleton.spawnAtLocation(stack);
+
+                                player.removeEffect(MobEffects.BAD_OMEN);
+
+                                player.addEffect(new MobEffectInstance(MobEffects.UNLUCK, 24000, 4));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
