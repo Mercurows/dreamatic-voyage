e3781763abbc3a8c19b831f9bf8520cd1505f5c7
Light_Quanta <zch22248@foxmail.com>
2024-04-17 14:02:57 +0800

    优化Optional写法

--- src/main/java/tech/lq0/dreamaticvoyage/item/magicros/chiram/ChiramLantern.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/magicros/chiram/ChiramLantern.java
@@ -1,6 +1,5 @@
 package tech.lq0.dreamaticvoyage.item.magicros.chiram;
 
-import com.google.common.util.concurrent.AtomicDouble;
 import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
@@ -13,7 +12,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.capability.ModCapabilities;
 import tech.lq0.dreamaticvoyage.capability.escort.EscortCapabilityProvider;
@@ -23,7 +21,6 @@ import tech.lq0.dreamaticvoyage.tools.Livers;
 import tech.lq0.dreamaticvoyage.tools.TooltipTool;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
 public class ChiramLantern extends Item {
@@ -61,64 +58,58 @@ public class ChiramLantern extends Item {
     @Override
     public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         if (entityIn instanceof Player player) {
-            if (player.getOffhandItem().getItem().equals(ItemRegistry.CHIRAM_LANTERN.get())) {
-                ItemStack lantern = player.getOffhandItem();
-                if (worldIn.getLightEmission(player.getOnPos()) <= 5) {
-                    LazyOptional<IEscortCapability> escortCapabilityLazyOptional = lantern.getCapability(ModCapabilities.ESCORT_CAPABILITY);
+            if (!player.getOffhandItem().getItem().equals(ItemRegistry.CHIRAM_LANTERN.get())) return;
 
-                    escortCapabilityLazyOptional.ifPresent(s -> {
-                        if (s.getEscortValue() < 200) {
-                            player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 300, 0, false, false), player);
-                            player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0, false, false));
-                            if (player.tickCount % 20 == 0 && !player.isCreative() && !player.isSpectator()) {
-                                s.addValue(1);
-                            }
-                        }
-                    });
+            ItemStack lantern = player.getOffhandItem();
+            if (worldIn.getLightEmission(player.getOnPos()) > 5) return;
+
+            lantern.getCapability(ModCapabilities.ESCORT_CAPABILITY).ifPresent(s -> {
+                if (s.getEscortValue() < 200) {
+                    player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 300, 0, false, false), player);
+                    player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0, false, false));
+                    if (player.tickCount % 20 == 0 && !player.isCreative() && !player.isSpectator()) {
+                        s.addValue(1);
+                    }
                 }
-            }
+            });
         }
     }
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if (pUsedHand == InteractionHand.MAIN_HAND) {
-            ItemStack stack = pPlayer.getMainHandItem();
-            ItemStack off = pPlayer.getOffhandItem();
+        if (pUsedHand != InteractionHand.MAIN_HAND) return super.use(pLevel, pPlayer, pUsedHand);
 
-            if (off.getItem() == Items.TORCH) {
-                LazyOptional<IEscortCapability> escortCapabilityLazyOptional = stack.getCapability(ModCapabilities.ESCORT_CAPABILITY);
+        ItemStack stack = pPlayer.getMainHandItem();
+        ItemStack off = pPlayer.getOffhandItem();
 
-                escortCapabilityLazyOptional.ifPresent(s -> {
-                    if (s.getEscortValue() > 0) {
-                        if (!pPlayer.isCreative() && !pPlayer.isSpectator()) {
-                            off.shrink(1);
-                        }
-                        s.setValue(0);
-                    }
-                });
+        if (off.getItem() != Items.TORCH) return super.use(pLevel, pPlayer, pUsedHand);
+
+        stack.getCapability(ModCapabilities.ESCORT_CAPABILITY).ifPresent(s -> {
+            if (s.getEscortValue() > 0) {
+                if (!pPlayer.isCreative() && !pPlayer.isSpectator()) {
+                    off.shrink(1);
+                }
+                s.setValue(0);
             }
-        }
+        });
 
         return super.use(pLevel, pPlayer, pUsedHand);
     }
 
     @Override
     public boolean isBarVisible(ItemStack pStack) {
-        AtomicBoolean flag = new AtomicBoolean(false);
-        LazyOptional<IEscortCapability> escortCapabilityLazyOptional = pStack.getCapability(ModCapabilities.ESCORT_CAPABILITY);
-        escortCapabilityLazyOptional.ifPresent(s -> flag.set(s.getEscortValue() > 0));
-
-        return flag.get();
+        return pStack.getCapability(ModCapabilities.ESCORT_CAPABILITY)
+                .map(s -> s.getEscortValue() > 0)
+                .orElse(false);
     }
 
     @Override
     public int getBarWidth(ItemStack pStack) {
-        AtomicDouble atomicDouble = new AtomicDouble(0);
-        LazyOptional<IEscortCapability> escortCapabilityLazyOptional = pStack.getCapability(ModCapabilities.ESCORT_CAPABILITY);
-        escortCapabilityLazyOptional.ifPresent(s -> atomicDouble.set(s.getEscortValue()));
-
-        return Math.round(13.0F - (float) atomicDouble.get() / 200f * 13.0F);
+        return pStack.getCapability(ModCapabilities.ESCORT_CAPABILITY)
+                .map(IEscortCapability::getEscortValue)
+                .map(Double::floatValue)
+                .map(v -> Math.round(13.0F - v / 200F * 13.0F))
+                .orElse(13);
     }
 
     @Override
