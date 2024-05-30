c0ccc9030d9ee145346ab10f1f3ca144d192220b
17146 <1714673995@qq.com>
2024-05-30 14:21:45 +0800

    添加奇迹证章的功能

--- src/main/java/tech/lq0/dreamaticvoyage/item/other/MiracleBadge.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/other/MiracleBadge.java
@@ -11,7 +11,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.tools.ArmorTool;
 import tech.lq0.dreamaticvoyage.tools.ItemNBTTool;
 import tech.lq0.dreamaticvoyage.tools.RarityTool;
@@ -19,11 +24,16 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class MiracleBadge extends Item implements ICurioItem {
     public static final String TAG_SET = "Set";
+
+    private static final List<String> players = Collections.synchronizedList(new ArrayList<>());
 
     public MiracleBadge() {
         super(new Properties().stacksTo(1).fireResistant());
@@ -76,5 +86,46 @@ public class MiracleBadge extends Item implements ICurioItem {
     @Override
     public Rarity getRarity(ItemStack pStack) {
         return ItemNBTTool.getBoolean(pStack, TAG_SET, false) ? RarityTool.MAGICROS : RarityTool.LEGENDARY;
+    }
+
+    @SubscribeEvent
+    public static void playerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
+        String username = event.getEntity().getGameProfile().getName();
+        players.remove(username + ":false");
+        players.remove(username + ":true");
+    }
+
+    private static String playerToString(Player player) {
+        return player.getGameProfile().getName() + ":" + player.level().isClientSide;
+    }
+
+    private static boolean checkFly(Player player) {
+        AtomicBoolean flag = new AtomicBoolean(false);
+        CuriosApi.getCuriosInventory(player).ifPresent(
+                c -> flag.set(c.findFirstCurio(ItemRegistry.MIRACLE_BADGE.get()).isPresent())
+        );
+        return flag.get();
+    }
+
+    @SubscribeEvent
+    public static void onLivingTick(LivingEvent.LivingTickEvent event) {
+        LivingEntity livingEntity = event.getEntity();
+        if (livingEntity instanceof Player player) {
+            if (players.contains(playerToString(player))) {
+                if (checkFly(player)) {
+                    player.getAbilities().mayfly = true;
+                } else {
+                    if (!player.isSpectator() && !player.isCreative()) {
+                        player.getAbilities().mayfly = false;
+                        player.getAbilities().flying = false;
+                        player.getAbilities().invulnerable = false;
+                    }
+                    players.remove(playerToString(player));
+                }
+            } else if (checkFly(player)) {
+                players.add(playerToString(player));
+                player.getAbilities().mayfly = true;
+            }
+        }
     }
 }
