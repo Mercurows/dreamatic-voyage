c2cac2e04b08a15a9ec297d17357dd392e48fab9
17146 <1714673995@qq.com>
2024-06-18 23:57:38 +0800

    添加沃土的交互逻辑

--- src/main/java/tech/lq0/dreamaticvoyage/block/VerdantSpiritRichSoilBlock.java
+++ src/main/java/tech/lq0/dreamaticvoyage/block/VerdantSpiritRichSoilBlock.java
@@ -1,19 +1,41 @@
 package tech.lq0.dreamaticvoyage.block;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+import tech.lq0.dreamaticvoyage.init.BlockRegistry;
 
 import java.util.List;
 
 public class VerdantSpiritRichSoilBlock extends Block {
     public VerdantSpiritRichSoilBlock() {
         super(Properties.of().mapColor(MapColor.DIRT).strength(0.5F).sound(SoundType.GRAVEL));
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        ItemStack stack = pPlayer.getItemInHand(pHand);
+
+        if (stack.is(ItemTags.HOES)) {
+            pLevel.setBlock(pPos, BlockRegistry.VERDANT_SPIRIT_FARMLAND.get().defaultBlockState(), 3);
+            return InteractionResult.SUCCESS;
+        }
+
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 
     @Override
