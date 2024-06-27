2927ddf8b6185c06a354d38d08fb4b1b933efa33
Light_Quanta <zch22248@foxmail.com>
2024-06-27 12:46:14 +0800

    为世界和平之杖添加唱片转换功能

--- src/main/java/tech/lq0/dreamaticvoyage/item/oi/shirako/WorldPeaceStaff.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/oi/shirako/WorldPeaceStaff.java
@@ -6,6 +6,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -15,16 +16,21 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.JukeboxBlock;
+import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.capability.ModCapabilities;
 import tech.lq0.dreamaticvoyage.capability.escort.EscortCapabilityProvider;
 import tech.lq0.dreamaticvoyage.entity.projectile.BloodCrystalEntity;
 import tech.lq0.dreamaticvoyage.init.EffectRegistry;
+import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.init.SoundRegistry;
 import tech.lq0.dreamaticvoyage.tiers.ModItemTier;
 import tech.lq0.dreamaticvoyage.tools.Livers;
@@ -42,6 +48,27 @@ public class WorldPeaceStaff extends SwordItem {
                 .rarity(RarityTool.LEGENDARY));
 
         this.energyCapacity = () -> 500.0;
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(UseOnContext pContext) {
+        if (pContext.getPlayer() == null || !pContext.getPlayer().isCrouching()) return super.useOn(pContext);
+
+        var level = pContext.getLevel();
+        var pos = pContext.getClickedPos();
+        var state = level.getBlockState(pos);
+
+        if (state.getBlock() instanceof JukeboxBlock) {
+            if (state.getValue(JukeboxBlock.HAS_RECORD)) {
+                var blockEntity = level.getBlockEntity(pos);
+                if (blockEntity instanceof JukeboxBlockEntity jukeboxBlockEntity) {
+                    jukeboxBlockEntity.setFirstItem(new ItemStack(ItemRegistry.MUSIC_DISC_AROUND_THE_TRAVEL.get()));
+                    return InteractionResult.SUCCESS;
+                }
+            }
+        }
+
+        return super.useOn(pContext);
     }
 
     @Override
