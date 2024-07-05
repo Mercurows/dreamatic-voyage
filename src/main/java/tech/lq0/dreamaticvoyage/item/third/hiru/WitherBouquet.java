02ef3c4519af59a60f2a1c0b8a46a62152766397
17146 <1714673995@qq.com>
2024-07-05 22:08:19 +0800

    修改了枯萎花束的效果

--- src/main/java/tech/lq0/dreamaticvoyage/item/third/hiru/WitherBouquet.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/third/hiru/WitherBouquet.java
@@ -1,18 +1,24 @@
 package tech.lq0.dreamaticvoyage.item.third.hiru;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.Utils;
 import tech.lq0.dreamaticvoyage.init.DamageSourceRegistry;
 import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.tools.Livers;
@@ -22,6 +28,7 @@ import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -47,20 +54,27 @@ public class WitherBouquet extends Item implements ICurioItem {
         return flag.get();
     }
 
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        Multimap<Attribute, AttributeModifier> map = ICurioItem.super.getAttributeModifiers(slotContext, uuid, stack);
+
+        map = HashMultimap.create(map);
+        map.put(Attributes.ATTACK_DAMAGE,
+                new AttributeModifier(uuid, Utils.MOD_ATTRIBUTE_MODIFIER, -0.2f, AttributeModifier.Operation.MULTIPLY_BASE));
+
+        return map;
+    }
+
     @SubscribeEvent
-    public static void onLivingHurt(LivingHurtEvent event) {
+    public static void onLivingDamaged(LivingDamageEvent event) {
         LivingEntity entity = event.getEntity();
 
         if (event.getSource().getEntity() instanceof Player player &&
                 event.getSource().is(DamageTypes.PLAYER_ATTACK)) {
 
             CuriosApi.getCuriosInventory(player).ifPresent(c -> c.findFirstCurio(ItemRegistry.WITHER_BOUQUET.get())
-                    .ifPresent(s -> {
-                        event.setAmount(event.getAmount() * 0.7f);
-                        entity.invulnerableTime = 0;
-                        entity.hurt(DamageSourceRegistry.causeAbsoluteWitherDamage(player.level().registryAccess(),
-                                player), event.getAmount() * 0.3f);
-                    }));
+                    .ifPresent(s -> entity.hurt(DamageSourceRegistry.causeAbsoluteWitherDamage(
+                            player.level().registryAccess(), player), event.getAmount() * 0.3f)));
         }
     }
 
