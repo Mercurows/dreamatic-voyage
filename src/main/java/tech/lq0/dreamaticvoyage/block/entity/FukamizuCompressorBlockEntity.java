1429fa45c4dece72c99edf738a1223ab09df5b2d
17146 <1714673995@qq.com>
2024-07-15 17:05:22 +0800

    添加基本的压缩机BE方法

--- src/main/java/tech/lq0/dreamaticvoyage/block/entity/FukamizuCompressorBlockEntity.java
+++ src/main/java/tech/lq0/dreamaticvoyage/block/entity/FukamizuCompressorBlockEntity.java
@@ -1,13 +1,197 @@
 package tech.lq0.dreamaticvoyage.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.init.BlockEntityRegistry;
+import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 
-public class FukamizuCompressorBlockEntity extends BlockEntity {
+public class FukamizuCompressorBlockEntity extends BlockEntity implements WorldlyContainer {
+    protected static final int SLOT_INPUT = 0;
+    protected static final int SLOT_RESULT = 1;
+
+    private static final int[] SLOTS_FOR_SIDES = new int[]{0};
+    private static final int[] SLOTS_FOR_DOWN = new int[]{1};
+
+    public static final int MIN_PRESSURE = 16;
+    public static final int MAX_PRESSURE = 24;
+
+    public static final int PROCESS_TIME = 5000;
+
+    protected NonNullList<ItemStack> items = NonNullList.withSize(2, ItemStack.EMPTY);
+
+    public int pressure;
+    public int compressingProgress;
+
+    protected final ContainerData dataAccess = new ContainerData() {
+        public int get(int pIndex) {
+            return switch (pIndex) {
+                case 0 -> FukamizuCompressorBlockEntity.this.pressure;
+                case 1 -> FukamizuCompressorBlockEntity.this.compressingProgress;
+                default -> 0;
+            };
+        }
+
+        public void set(int pIndex, int pValue) {
+            switch (pIndex) {
+                case 0:
+                    FukamizuCompressorBlockEntity.this.pressure = pValue;
+                    break;
+                case 1:
+                    FukamizuCompressorBlockEntity.this.compressingProgress = pValue;
+                    break;
+            }
+        }
+
+        public int getCount() {
+            return 2;
+        }
+    };
 
     public FukamizuCompressorBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(BlockEntityRegistry.FUKAMIZU_COMPRESSOR_BLOCK_ENTITY.get(), pPos, pBlockState);
+    }
+
+    public static void serverTick(Level pLevel, BlockPos pPos, BlockState pState, FukamizuCompressorBlockEntity blockEntity) {
+        blockEntity.pressure = calculatePressure(pLevel, pPos);
+
+        if (blockEntity.canProcess()) {
+            // TODO 完成加工逻辑
+
+
+        }
+
+    }
+
+    private static int calculatePressure(Level pLevel, BlockPos pPos) {
+        int count = 0;
+
+        while (pLevel.getBlockState(pPos.above()).is(Blocks.WATER)) {
+            count++;
+            pPos = pPos.above();
+        }
+        return count;
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(pTag, this.items);
+        this.pressure = pTag.getInt("Pressure");
+        this.compressingProgress = pTag.getInt("CompressingProgress");
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        super.saveAdditional(pTag);
+
+        ContainerHelper.saveAllItems(pTag, this.items);
+        pTag.putInt("Pressure", this.pressure);
+        pTag.putInt("CompressingProgress", this.compressingProgress);
+    }
+
+    private boolean canProcess() {
+        return this.pressure >= MIN_PRESSURE && this.pressure <= MAX_PRESSURE;
+    }
+
+    @Override
+    public int[] getSlotsForFace(Direction pSide) {
+        if (pSide == Direction.DOWN) {
+            return SLOTS_FOR_DOWN;
+        } else if (pSide != Direction.UP) {
+            return SLOTS_FOR_SIDES;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean canPlaceItemThroughFace(int pIndex, ItemStack pItemStack, @Nullable Direction pDirection) {
+        return this.canPlaceItem(pIndex, pItemStack);
+    }
+
+    public boolean canPlaceItem(int pIndex, ItemStack pStack) {
+        if (pIndex == 1) {
+            return false;
+        } else return pIndex == 0 && pStack.is(ItemRegistry.FUKAMIZU_BREAD_INGOT.get());
+    }
+
+    @Override
+    public boolean canTakeItemThroughFace(int pIndex, ItemStack pStack, Direction pDirection) {
+        if (pDirection == Direction.DOWN && pIndex == 1) {
+            return pStack.is(ItemRegistry.SWOLLEN_FUKAMIZU_BREAD_INGOT.get());
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public int getContainerSize() {
+        return this.items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (ItemStack itemstack : this.items) {
+            if (!itemstack.isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public ItemStack getItem(int pSlot) {
+        return this.items.get(pSlot);
+    }
+
+    @Override
+    public ItemStack removeItem(int pSlot, int pAmount) {
+        return ContainerHelper.removeItem(this.items, pSlot, pAmount);
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int pSlot) {
+        return ContainerHelper.takeItem(this.items, pSlot);
+    }
+
+    @Override
+    public void setItem(int pSlot, ItemStack pStack) {
+        ItemStack itemstack = this.items.get(pSlot);
+        boolean flag = !pStack.isEmpty() && ItemStack.isSameItemSameTags(itemstack, pStack);
+        this.items.set(pSlot, pStack);
+        if (pStack.getCount() > this.getMaxStackSize()) {
+            pStack.setCount(this.getMaxStackSize());
+        }
+
+        if (pSlot == 0 && !flag) {
+            this.compressingProgress = 0;
+            this.setChanged();
+        }
+    }
+
+    @Override
+    public boolean stillValid(Player pPlayer) {
+        return Container.stillValidBlockEntity(this, pPlayer);
+    }
+
+    @Override
+    public void clearContent() {
+        this.items.clear();
     }
 }
--- src/main/java/tech/lq0/dreamaticvoyage/block/fukamizutech/FukamizuCompressor.java
+++ src/main/java/tech/lq0/dreamaticvoyage/block/fukamizutech/FukamizuCompressor.java
@@ -2,15 +2,20 @@ package tech.lq0.dreamaticvoyage.block.fukamizutech;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.block.entity.FukamizuCompressorBlockEntity;
+import tech.lq0.dreamaticvoyage.init.BlockEntityRegistry;
 
 public class FukamizuCompressor extends Block implements EntityBlock {
     private static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
@@ -22,7 +27,7 @@ public class FukamizuCompressor extends Block implements EntityBlock {
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
-        return null;
+        return new FukamizuCompressorBlockEntity(pPos, pState);
     }
 
     @Override
@@ -34,5 +39,19 @@ public class FukamizuCompressor extends Block implements EntityBlock {
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
         return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if (!pLevel.isClientSide) {
+            return createTickerHelper(pBlockEntityType, BlockEntityRegistry.FUKAMIZU_COMPRESSOR_BLOCK_ENTITY.get(), FukamizuCompressorBlockEntity::serverTick);
+        }
+        return null;
+    }
+
+    @Nullable
+    protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> createTickerHelper(BlockEntityType<A> pServerType, BlockEntityType<E> pClientType, BlockEntityTicker<? super E> pTicker) {
+        return pClientType == pServerType ? (BlockEntityTicker<A>) pTicker : null;
     }
 }
