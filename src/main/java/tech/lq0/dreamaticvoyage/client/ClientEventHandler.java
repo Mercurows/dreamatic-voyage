d5d818ccf0787cdd58d54a4400b0f03eed6dda1c
17146 <1714673995@qq.com>
2024-07-15 23:03:46 +0800

    修改注册screen的总线

--- src/main/java/tech/lq0/dreamaticvoyage/client/ClientEventHandler.java
+++ src/main/java/tech/lq0/dreamaticvoyage/client/ClientEventHandler.java
@@ -1,17 +1,13 @@
 package tech.lq0.dreamaticvoyage.client;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import tech.lq0.dreamaticvoyage.client.screen.FukamizuCompressorScreen;
-import tech.lq0.dreamaticvoyage.init.MenuTypeRegistry;
 import tech.lq0.dreamaticvoyage.tools.AnimationTickHolder;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class ClientEventHandler {
 
     @SubscribeEvent
@@ -26,12 +22,5 @@ public class ClientEventHandler {
     //From Create
     protected static boolean isGameActive() {
         return !(Minecraft.getInstance().level == null || Minecraft.getInstance().player == null);
-    }
-
-    @SubscribeEvent
-    public static void onClientSetUpEvent(FMLClientSetupEvent event) {
-        event.enqueueWork(() -> {
-            MenuScreens.register(MenuTypeRegistry.FUKAMIZU_COMPRESSOR_MENU.get(), FukamizuCompressorScreen::new);
-        });
     }
 }
---
+++ src/main/java/tech/lq0/dreamaticvoyage/init/ScreenRegistry.java
@@ -0,0 +1,19 @@
+package tech.lq0.dreamaticvoyage.init;
+
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import tech.lq0.dreamaticvoyage.client.screen.FukamizuCompressorScreen;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ScreenRegistry {
+
+    @SubscribeEvent
+    public static void onClientSetUpEvent(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            MenuScreens.register(MenuTypeRegistry.FUKAMIZU_COMPRESSOR_MENU.get(), FukamizuCompressorScreen::new);
+        });
+    }
+}
