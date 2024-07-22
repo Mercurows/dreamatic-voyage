9bad94458ef4a00b1e65e28f757ffe6d46009097
17146 <1714673995@qq.com>
2024-07-23 03:47:04 +0800

    修改矿化深水面包的使用方式

--- src/main/java/tech/lq0/dreamaticvoyage/item/misc/fukamizutech/MineralizedFukamizuBread.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/misc/fukamizutech/MineralizedFukamizuBread.java
@@ -2,10 +2,9 @@ package tech.lq0.dreamaticvoyage.item.misc.fukamizutech;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
@@ -14,14 +13,20 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.init.DamageSourceRegistry;
 import tech.lq0.dreamaticvoyage.tools.TooltipTool;
 
 import java.util.List;
 import java.util.Random;
 
 public class MineralizedFukamizuBread extends Item {
+    private static final FoodProperties food = (new FoodProperties.Builder()).saturationMod(0.75f).nutrition(6)
+            .alwaysEat().build();
+
+    private static List<Item> rawMaterials = List.of();
+
     public MineralizedFukamizuBread() {
-        super(new Properties().rarity(Rarity.UNCOMMON).fireResistant());
+        super(new Properties().rarity(Rarity.UNCOMMON).fireResistant().food(food));
     }
 
     @Override
@@ -31,16 +36,15 @@ public class MineralizedFukamizuBread extends Item {
         TooltipTool.addDevelopingText(pTooltipComponents);
     }
 
-    private static List<Item> rawMaterials = List.of();
+    @Override
+    public int getUseDuration(ItemStack stack) {
+        return 80;
+    }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if (!pLevel.isClientSide && pLevel instanceof ServerLevel) {
-            ItemStack stack = pPlayer.getItemInHand(pUsedHand);
-
-            if (!pPlayer.getAbilities().instabuild) {
-                stack.shrink(1);
-            }
+    public ItemStack finishUsingItem(ItemStack stack, Level pLevel, LivingEntity pLivingEntity) {
+        if (!pLevel.isClientSide && pLivingEntity instanceof Player player) {
+            pLivingEntity.hurt(DamageSourceRegistry.causeFukamizuBreadDamage(pLevel.registryAccess(), null), 10.0f);
 
             if (rawMaterials.isEmpty()) {
                 rawMaterials = ForgeRegistries.ITEMS.getValues()
@@ -49,10 +53,15 @@ public class MineralizedFukamizuBread extends Item {
                         .toList();
             }
 
-            int index = new Random().nextInt(rawMaterials.size());
-            pPlayer.addItem(new ItemStack(rawMaterials.get(index)));
+            double random = new Random().nextDouble();
+
+            if (random > .25) {
+                int index = new Random().nextInt(rawMaterials.size());
+                player.addItem(new ItemStack(rawMaterials.get(index)));
+            }
         }
 
-        return super.use(pLevel, pPlayer, pUsedHand);
+        return super.finishUsingItem(stack, pLevel, pLivingEntity);
     }
+
 }
