7add2e6b385e27442367cab0b57d928dcb1b5ccc
17146 <1714673995@qq.com>
2024-07-24 03:25:37 +0800

    添加kuriko的贴图

--- src/main/java/tech/lq0/dreamaticvoyage/init/BlockEntityRegistry.java
+++ src/main/java/tech/lq0/dreamaticvoyage/init/BlockEntityRegistry.java
@@ -5,10 +5,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import tech.lq0.dreamaticvoyage.Utils;
-import tech.lq0.dreamaticvoyage.block.entity.FukamizuCompressorBlockEntity;
-import tech.lq0.dreamaticvoyage.block.entity.PhantasmalVoyagerBlockEntity;
-import tech.lq0.dreamaticvoyage.block.entity.PointsStoreBlockEntity;
-import tech.lq0.dreamaticvoyage.block.entity.VerdantSpiritFarmlandBlockEntity;
+import tech.lq0.dreamaticvoyage.block.entity.*;
 
 public class BlockEntityRegistry {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, Utils.MOD_ID);
@@ -22,6 +19,10 @@ public class BlockEntityRegistry {
     public static final RegistryObject<BlockEntityType<VerdantSpiritFarmlandBlockEntity>> VERDANT_SPIRIT_FARMLAND_BLOCK_ENTITY =
             BLOCK_ENTITY_TYPES.register("verdant_spirit_farmland_block_entity",
                     () -> BlockEntityType.Builder.of(VerdantSpiritFarmlandBlockEntity::new, BlockRegistry.VERDANT_SPIRIT_FARMLAND.get()).build(null));
+
+    public static final RegistryObject<BlockEntityType<CrystalPopperBlockEntity>> CRYSTAL_POPPER_BLOCK_ENTITY =
+            BLOCK_ENTITY_TYPES.register("crystal_popper_block_entity",
+                    () -> BlockEntityType.Builder.of(CrystalPopperBlockEntity::new, BlockRegistry.CRYSTAL_POPPER.get()).build(null));
 
     public static final RegistryObject<BlockEntityType<FukamizuCompressorBlockEntity>> FUKAMIZU_COMPRESSOR_BLOCK_ENTITY =
             BLOCK_ENTITY_TYPES.register("fukamizu_compressor_block_entity",
