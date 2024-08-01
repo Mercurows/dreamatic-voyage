f93974b88db214499df54a9f0943bfd981067298
17146 <1714673995@qq.com>
2024-08-01 15:17:00 +0800

    修改创意破产的获取方式

--- src/main/java/tech/lq0/dreamaticvoyage/effect/CreativityBankruptcy.java
+++ src/main/java/tech/lq0/dreamaticvoyage/effect/CreativityBankruptcy.java
@@ -19,10 +19,10 @@ import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import tech.lq0.dreamaticvoyage.compat.vrc.VrcHolder;
 import tech.lq0.dreamaticvoyage.init.EffectRegistry;
@@ -37,21 +37,6 @@ public class CreativityBankruptcy extends MobEffect {
         super(MobEffectCategory.HARMFUL, 0x87EAFF);
     }
 
-    @SubscribeEvent
-    public static void onAttackEntity(AttackEntityEvent event) {
-        Player player = event.getEntity();
-
-        if (event.getTarget() instanceof Pig pig) {
-            List<Villager> list = pig.level().getEntitiesOfClass(Villager.class, pig.getBoundingBox().inflate(8)).stream()
-                    .filter(villager -> villager.getVillagerData().getProfession() == VillagerRegistry.ELIFAUS.get())
-                    .toList();
-
-            if (!list.isEmpty()) {
-                player.addEffect(new MobEffectInstance(EffectRegistry.CREATIVITY_BANKRUPTCY.get(), 600 * list.size(), 0));
-            }
-        }
-    }
-
     @SuppressWarnings("ConstantValue")
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onInteractEntity(PlayerInteractEvent.EntityInteract event) {
@@ -59,16 +44,18 @@ public class CreativityBankruptcy extends MobEffect {
         Entity entity = event.getTarget();
 
         if (entity instanceof Villager villager && villager.getVillagerData().getProfession() == VillagerRegistry.ELIFAUS.get()) {
-            List<ItemStack> stacks = player.getInventory().items.stream().filter(stack -> stack.is(TagKey.create(Registries.ITEM,
-                    new ResourceLocation("virtuarealcraft", "sui")))).toList();
+            if (ModList.get().isLoaded("virtuarealcraft")) {
+                List<ItemStack> stacks = player.getInventory().items.stream().filter(stack -> stack.is(TagKey.create(Registries.ITEM,
+                        new ResourceLocation("virtuarealcraft", "sui")))).toList();
 
-            int count = 0;
-            for (ItemStack stack : stacks) {
-                count += stack.getCount();
-            }
+                int count = 0;
+                for (ItemStack stack : stacks) {
+                    count += stack.getCount();
+                }
 
-            if (count != 0) {
-                player.addEffect(new MobEffectInstance(EffectRegistry.CREATIVITY_BANKRUPTCY.get(), count * 20, 0));
+                if (count != 0) {
+                    player.addEffect(new MobEffectInstance(EffectRegistry.CREATIVITY_BANKRUPTCY.get(), count * 20, 0));
+                }
             }
         }
 
