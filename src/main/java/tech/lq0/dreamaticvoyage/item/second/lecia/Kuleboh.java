b70b0f8fe4ddb72acb30661c2e07f334f8b70e59
17146 <1714673995@qq.com>
2024-09-09 11:33:04 +0800

    修改吊坠配方，优化莉子球转化方式

--- src/main/java/tech/lq0/dreamaticvoyage/item/second/lecia/Kuleboh.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/second/lecia/Kuleboh.java
@@ -2,16 +2,19 @@ package tech.lq0.dreamaticvoyage.item.second.lecia;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingUseTotemEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
 import tech.lq0.dreamaticvoyage.init.ItemRegistry;
 import tech.lq0.dreamaticvoyage.tools.ItemNBTTool;
@@ -20,10 +23,9 @@ import tech.lq0.dreamaticvoyage.tools.TooltipTool;
 
 import java.util.List;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class Kuleboh extends Item {
-    public static final String TAG_LOCKED = "locked";
-    public static final String TAG_TOTEM_COUNT = "totem_count";
-    public static final String TAG_NOW_COUNT = "now_count";
+    public static final String TAG_TOTEM_COUNT = "UsedTotemCount";
 
     private static final FoodProperties food = new FoodProperties.Builder().nutrition(5).saturationMod(0.5f)
             .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 1200, 1), 1.0f)
@@ -41,30 +43,14 @@ public class Kuleboh extends Item {
     }
 
     @Override
-    public void inventoryTick(ItemStack stack, Level worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
-        if (entityIn instanceof Player player) {
-            if (!worldIn.isClientSide) {
-                ServerPlayer serverPlayer = (ServerPlayer) player;
-                int count = serverPlayer.getStats().getValue(Stats.ITEM_USED.get(Items.TOTEM_OF_UNDYING));
-
-                if (stack != player.getOffhandItem()) {
-                    setLocked(stack, false);
+    public void inventoryTick(ItemStack stack, Level level, Entity entityIn, int itemSlot, boolean isSelected) {
+        if (entityIn instanceof Player player && !level.isClientSide) {
+            if (player.getOffhandItem() == stack) {
+                if (getTotemCount(stack) >= 5) {
+                    player.setItemSlot(EquipmentSlot.OFFHAND, new ItemStack(ItemRegistry.KULECIABOH.get()));
                 }
-
-                //初始化并赋值
-                if (!isLocked(stack)) {
-                    setTotemCount(stack, count + 5);
-                    setNowCount(stack, count);
-                    setLocked(stack, true);
-                } else {
-                    if (stack == player.getOffhandItem()) {
-                        setNowCount(stack, count);
-
-                        if (getNowCount(stack) >= getTotemCount(stack)) {
-                            player.setItemSlot(EquipmentSlot.OFFHAND, new ItemStack(ItemRegistry.KULECIABOH.get()));
-                        }
-                    }
-                }
+            } else {
+                setTotemCount(stack, 0);
             }
         }
     }
@@ -74,27 +60,22 @@ public class Kuleboh extends Item {
         return oldStack != newStack && slotChanged;
     }
 
+    @SubscribeEvent
+    public static void onPlayerUseTotem(LivingUseTotemEvent event) {
+        LivingEntity living = event.getEntity();
+        if (living instanceof Player player && event.getHandHolding() == InteractionHand.MAIN_HAND) {
+            ItemStack stack = player.getOffhandItem();
+            if (stack.getItem() == ItemRegistry.KULEBOH.get()) {
+                setTotemCount(stack, getTotemCount(stack) + 1);
+            }
+        }
+    }
+
     public static void setTotemCount(ItemStack stack, int count) {
         ItemNBTTool.setInt(stack, TAG_TOTEM_COUNT, count);
     }
 
     public static int getTotemCount(ItemStack stack) {
         return ItemNBTTool.getInt(stack, TAG_TOTEM_COUNT, 0);
-    }
-
-    public static void setLocked(ItemStack stack, boolean flag) {
-        ItemNBTTool.setBoolean(stack, TAG_LOCKED, flag);
-    }
-
-    public static boolean isLocked(ItemStack stack) {
-        return ItemNBTTool.getBoolean(stack, TAG_LOCKED, false);
-    }
-
-    public static void setNowCount(ItemStack stack, int count) {
-        ItemNBTTool.setInt(stack, TAG_NOW_COUNT, count);
-    }
-
-    public static int getNowCount(ItemStack stack) {
-        return ItemNBTTool.getInt(stack, TAG_NOW_COUNT, 0);
     }
 }
