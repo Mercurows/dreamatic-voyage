50d8a815604f61343cb9c466ca859808711673e3
17146 <1714673995@qq.com>
2024-10-03 21:16:26 +0800

    修改魔力充盈的实现方式

--- src/main/java/tech/lq0/dreamaticvoyage/effect/ManaSurge.java
+++ src/main/java/tech/lq0/dreamaticvoyage/effect/ManaSurge.java
@@ -1,16 +1,13 @@
 package tech.lq0.dreamaticvoyage.effect;
 
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tech.lq0.dreamaticvoyage.init.EffectRegistry;
+import tech.lq0.dreamaticvoyage.tools.ModTags;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ManaSurge extends MobEffect {
@@ -19,37 +16,12 @@ public class ManaSurge extends MobEffect {
     }
 
     @SubscribeEvent
-    public static void onEffectAdded(MobEffectEvent.Added event) {
-        LivingEntity living = event.getEntity();
-        if (event.getEffectSource() instanceof LivingEntity source) {
-            if (source.hasEffect(EffectRegistry.MANA_SURGE.get())) {
-                MobEffectInstance instance = event.getEffectInstance();
-                if (instance.getEffect().equals(EffectRegistry.MANA_SURGE.get())) {
-                    return;
-                }
-
-                if (instance.getEffect().getCategory() != MobEffectCategory.BENEFICIAL) {
-                    if (!living.equals(source)) {
-                        living.removeEffect(instance.getEffect());
-                        living.addEffect(new MobEffectInstance(
-                                instance.getEffect(), instance.getDuration(), instance.getAmplifier() + 1,
-                                instance.isAmbient(), instance.isVisible(), instance.showIcon()), null);
-                    }
-                } else {
-                    living.removeEffect(instance.getEffect());
-                    living.addEffect(new MobEffectInstance(
-                            instance.getEffect(), instance.getDuration(), instance.getAmplifier() + 1,
-                            instance.isAmbient(), instance.isVisible(), instance.showIcon()), null);
-                }
-            }
-        }
-    }
-
-    @SubscribeEvent
     public static void onLivingHurt(LivingHurtEvent event) {
         DamageSource source = event.getSource();
-        if (source.is(DamageTypes.MAGIC) && event.getEntity().hasEffect(EffectRegistry.MANA_SURGE.get())) {
-            event.setAmount(event.getAmount() / 2.0f);
+        if (source.is(ModTags.DamageTypes.MAGIC_CAN_BE_ENHANCED) && event.getEntity().hasEffect(EffectRegistry.MANA_SURGE.get())) {
+            var instance = event.getEntity().getEffect(EffectRegistry.MANA_SURGE.get());
+            assert instance != null;
+            event.setAmount((float) (event.getAmount() * (0.9 * (1 - 1 / Math.pow(2, instance.getAmplifier() + 0.5)))));
         }
     }
 }
--- src/main/java/tech/lq0/dreamaticvoyage/mixin/LivingEntityMixin.java
+++ src/main/java/tech/lq0/dreamaticvoyage/mixin/LivingEntityMixin.java
@@ -1,6 +1,10 @@
 package tech.lq0.dreamaticvoyage.mixin;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.common.MinecraftForge;
@@ -8,6 +12,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tech.lq0.dreamaticvoyage.init.EffectRegistry;
 import tech.lq0.dreamaticvoyage.network.event.LivingItemBreakEvent;
 
 @Mixin(LivingEntity.class)
@@ -24,6 +30,33 @@ public abstract class LivingEntityMixin {
     public void broadcastBreakEvent(InteractionHand pHand, CallbackInfo ci) {
         if ((Object) this instanceof LivingEntity living) {
             MinecraftForge.EVENT_BUS.post(new LivingItemBreakEvent(living, pHand));
+        }
+    }
+
+    @Inject(method = "addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z", at = @At("HEAD"))
+    public void addEffect(MobEffectInstance pEffectInstance, Entity pEntity, CallbackInfoReturnable<Boolean> cir) {
+        CompoundTag newNBT = new CompoundTag();
+        pEffectInstance.save(newNBT);
+        int i = newNBT.getByte("Amplifier");
+        if (pEntity instanceof LivingEntity source) {
+            if (source.hasEffect(EffectRegistry.MANA_SURGE.get())) {
+                int level = source.getEffect(EffectRegistry.MANA_SURGE.get()).getAmplifier();
+                i = Math.min(127, i + level);
+                if (!pEffectInstance.getEffect().equals(EffectRegistry.MANA_SURGE.get())) {
+                    if (pEffectInstance.getEffect().getCategory() != MobEffectCategory.BENEFICIAL) {
+                        LivingEntity living = (LivingEntity) (Object) this;
+                        if (!living.equals(source)) {
+                            newNBT.putByte("Amplifier", (byte) i);
+                        }
+                    } else {
+                        newNBT.putByte("Amplifier", (byte) i);
+                    }
+                    var instance = MobEffectInstance.load(newNBT);
+                    if (instance != null) {
+                        pEffectInstance.update(instance);
+                    }
+                }
+            }
         }
     }
 
