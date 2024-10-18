789d7eefe5f743d53a92067d05d8562c5c9f7729
17146 <1714673995@qq.com>
2024-10-18 16:51:43 +0800

    添加了锯齿锋芒的功能

--- src/main/java/tech/lq0/dreamaticvoyage/item/oi/fukami/SerrationEdge.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/oi/fukami/SerrationEdge.java
@@ -1,9 +1,27 @@
 package tech.lq0.dreamaticvoyage.item.oi.fukami;
 
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.SwordItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import tech.lq0.dreamaticvoyage.init.EffectRegistry;
 import tech.lq0.dreamaticvoyage.tiers.ModItemTier;
+import tech.lq0.dreamaticvoyage.tools.Livers;
+import tech.lq0.dreamaticvoyage.tools.TooltipTool;
+
+import java.util.List;
 
 public class SerrationEdge extends SwordItem {
 
@@ -12,7 +30,89 @@ public class SerrationEdge extends SwordItem {
     }
 
     @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.serration_edge_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.serration_edge_2").withStyle(ChatFormatting.GRAY));
+
+        TooltipTool.addLiverInfo(pTooltipComponents, Livers.FUKAMIZU);
+//        handleUpgradeTooltips(pStack, pTooltipComponents);
+    }
+
+    @Override
     public boolean isDamageable(ItemStack stack) {
         return false;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack pStack) {
+        return 100;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
+        pPlayer.startUsingItem(pUsedHand);
+        return InteractionResultHolder.consume(stack);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        if (!pTarget.level().isClientSide && pTarget.level().random.nextDouble() < .5) {
+            pTarget.addEffect(new MobEffectInstance(EffectRegistry.BLEEDING.get(), 100,
+                    pTarget.level().random.nextIntBetweenInclusive(0, 2)), pAttacker);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+        if (pLivingEntity instanceof Player player) {
+            if (!pLevel.isClientSide) {
+                HitResult result = getHitResult(pLevel, player);
+                if (result.getType() == HitResult.Type.ENTITY) {
+                    EntityHitResult hitResult = (EntityHitResult) result;
+                    if (hitResult.getEntity() instanceof LivingEntity living) {
+                        living.hurt(pLevel.damageSources().playerAttack(player), 1.5f);
+                        living.invulnerableTime = 0;
+                    }
+                }
+            }
+        }
+        super.onUseTick(pLevel, pLivingEntity, pStack, pRemainingUseDuration);
+    }
+
+    private HitResult getHitResult(Level level, Player player) {
+        double entityReach = player.getEntityReach();
+        HitResult rayTrace = Item.getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
+        Vec3 vec = player.getEyePosition(1.0f);
+
+        double distance = rayTrace.getLocation().distanceToSqr(vec);
+        Vec3 viewVector = player.getViewVector(1.0f);
+        Vec3 addedVec = vec.add(viewVector.x * entityReach, viewVector.y * entityReach, viewVector.z * entityReach);
+
+        AABB aabb = player.getBoundingBox().expandTowards(viewVector.scale(entityReach)).inflate(1.0d, 1.0d, 1.0d);
+        EntityHitResult hitResult = ProjectileUtil.getEntityHitResult(player, vec, addedVec, aabb, entity -> !entity.isSpectator() && entity.isPickable(), distance);
+        if (hitResult != null) {
+            rayTrace = hitResult;
+        }
+        return rayTrace;
+    }
+
+    @Override
+    public void onStopUsing(ItemStack stack, LivingEntity entity, int count) {
+        if (!entity.level().isClientSide && entity instanceof Player player) {
+            player.getCooldowns().addCooldown(stack.getItem(), (getUseDuration(stack) - count) * 6);
+        }
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack pStack) {
+        return UseAnim.BOW;
+    }
+
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+        return slotChanged;
     }
 }
