f4620ca138a798e4d990924257d80bf56d1dca8f
17146 <1714673995@qq.com>
2024-10-30 21:45:53 +0800

    修改文案

--- src/main/java/tech/lq0/dreamaticvoyage/item/misc/material/ingot/GachiPowder.java
+++ src/main/java/tech/lq0/dreamaticvoyage/item/misc/material/ingot/GachiPowder.java
@@ -1,12 +1,26 @@
 package tech.lq0.dreamaticvoyage.item.misc.material.ingot;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GachiPowder extends Item {
 
     public GachiPowder() {
         super(new Properties().rarity(Rarity.EPIC));
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.gachi_powder_1").withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("des.dreamaticvoyage.gachi_powder_2").withStyle(ChatFormatting.GRAY));
     }
 
 }
